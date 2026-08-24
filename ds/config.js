36368e092783b7000d162632442bf467855a13c7
VahiDArmy <vahid8910@gmail.com>
2026-08-24 22:28:56 +0330

    Create config.js

---
+++ ds/config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = 'https://kakjzcqfaaazgtmaqcgg.supabase.co';
+const SUPABASE_ANON_KEY = 'https://kakjzcqfaaazgtmaqcgg.supabase.co';
+const ADMIN_UUID = '1fc6d3d5-8cec-4578-bcea-c619dcdb37d7'; // اختیاری، برای بررسی در کلاینت
