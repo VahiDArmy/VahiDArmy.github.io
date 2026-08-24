fd4ca7439b4256bf8be555d06317a73863c5c812
VahiDArmy <vahid8910@gmail.com>
2026-08-24 22:56:29 +0330

    Create config.js

---
+++ ds/js/config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://kakjzcqfaaazgtmaqcgg.supabase.co';
+const SUPABASE_ANON_KEY = 'https://kakjzcqfaaazgtmaqcgg.supabase.co';
+const ADMIN_UUID = '1fc6d3d5-8cec-4578-bcea-c619dcdb37d7'; // اختیاری، برای بررسی در کلاینت
+const DATA_PATH = '../quran/'; // مسیر نسبی پوشه داده‌ها از ریشه سایت
