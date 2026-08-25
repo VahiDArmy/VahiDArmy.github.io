76f1b9a42c48ac4d71613e4b9a05683f94981716
VahiDArmy <vahid8910@gmail.com>
2026-08-25 17:11:59 +0330

    Update config.js via Neon FM

--- ds/js/config.js
+++ ds/js/config.js
@@ -2,3 +2,4 @@ const SUPABASE_URL = 'https://kakjzcqfaaazgtmaqcgg.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_LGxGcIC45lvZeDRj0-QWEg_f8jt06LV';
 const ADMIN_UUID = '542cfcbe-cfc9-4cce-9977-4da5e613c1d5'; // اختیاری، برای بررسی در کلاینت
 const DATA_PATH = '../c/data/surahs/';
+const TOTAL_AYAHS_IN_QURAN = 6236; // تعداد کل آیات
