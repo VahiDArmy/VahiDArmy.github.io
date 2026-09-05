1f108cfa232a8765accafe9075d16846b3d5e69c
VahiDArmy <vahid8910@gmail.com>
2026-09-05 23:10:00 +0330

    Update food/js/config.js

--- food/js/config.js
+++ food/js/config.js
@@ -1,2 +1,9 @@
+// Import createClient از CDN (برای استفاده در ماژول)
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2';
+
+// کلیدهای پروژه‌ی خود را جایگزین کنید
 export const SUPABASE_URL = 'https://tmjctucdptckeuwguzdm.supabase.co';
 export const SUPABASE_ANON_KEY = 'sb_publishable_S9skokNcDPidkoEdtZe_0g_jPHh_b4l';
+
+// ساخت کلاینت و صادرات آن
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
