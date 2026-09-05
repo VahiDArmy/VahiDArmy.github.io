7b9225c54d3dd56a386a84e7c4c5a81cc0d0ca35
VahiDArmy <vahid8910@gmail.com>
2026-09-05 23:21:47 +0330

    Update food/js/config.js

--- food/js/config.js
+++ food/js/config.js
@@ -2,5 +2,5 @@
 export const SUPABASE_URL = 'https://tmjctucdptckeuwguzdm.supabase.co';
 export const SUPABASE_ANON_KEY = 'sb_publishable_S9skokNcDPidkoEdtZe_0g_jPHh_b4l';
 
-// ساخت کلاینت و صادرات آن
+// از شیء سراسری supabase که توسط اسکریپت CDN ساخته شده استفاده می‌کنیم
 export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
