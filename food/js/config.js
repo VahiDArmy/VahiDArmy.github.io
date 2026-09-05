939128a38fe2e2b923b0eb099a61bf365e8d734c
VahiDArmy <vahid8910@gmail.com>
2026-09-05 20:34:17 +0330

    Update food/js/config.js

--- food/js/config.js
+++ food/js/config.js
@@ -1,7 +1,3 @@
-const SUPABASE_URL = "https://tmjctucdptckeuwguzdm.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_S9skokNcDPidkoEdtZe_0g_jPHh_b4l";
-
-const supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
+// تنظیمات Supabase
+export const SUPABASE_URL = 'https://tmjctucdptckeuwguzdm.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_S9skokNcDPidkoEdtZe_0g_jPHh_b4l';
